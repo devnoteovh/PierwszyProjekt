5d739be6695e379c61b7c7523ca9de519b4cb679
devnoteovh <devnoteovh@users.noreply.github.com>
2020-10-13 13:58:12 +0200

    Commit: New line with sout + new catalog ignore

--- src/ovh/devnote/pierwszyprojekt/Main.java
+++ src/ovh/devnote/pierwszyprojekt/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Pierwszy projekt");
+        System.out.println("dodalem zmiane do projektu");
     }
 }
